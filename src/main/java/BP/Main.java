5b4175250ea403361907af240e2c0908f2741e6c
Manuel Quispe Cueva <mquispecueva1@upao.edu.pe>
2024-06-08 05:15:33 -0500

    arreglao

--- src/main/java/BP/Main.java
+++ src/main/java/BP/Main.java
@@ -1,12 +1,9 @@
 package BP;
 
-import BP.infrastructure.config.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({FileStorageProperties.class})
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
