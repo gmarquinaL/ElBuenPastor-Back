85ea05716674676317c8d69ccf8d01e1714254b4
Manuel Quispe Cueva <mquispecueva1@upao.edu.pe>
2024-06-28 00:38:02 -0500

    prueba

---
+++ src/main/java/BP/infrastructure/config/FileUploadConfig.java
@@ -0,0 +1,19 @@
+package BP.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.util.unit.DataSize;
+import jakarta.servlet.MultipartConfigElement;
+
+@Configuration
+public class FileUploadConfig {
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize(DataSize.ofMegabytes(10));
+        factory.setMaxRequestSize(DataSize.ofMegabytes(10));
+        return factory.createMultipartConfig();
+    }
+}
